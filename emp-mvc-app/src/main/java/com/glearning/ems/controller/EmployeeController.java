0b643e54524de4d341a11a8add3791a5c156b104
aditi1986 <103111483+aditi1986@users.noreply.github.com>
2023-07-05 15:48:17 +0530

    Add files via upload

---
+++ emp-mvc-app/src/main/java/com/glearning/ems/controller/EmployeeController.java
@@ -0,0 +1,53 @@
+package com.glearning.ems.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.glearning.ems.model.Employee;
+import com.glearning.ems.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeservice;
+
+	@GetMapping("/listEmployees")
+	public String viewallEmployees(Model model) {
+		List<Employee> viewallEmployees = this.employeeservice.viewAllEmployee();
+		model.addAttribute("viewallEmployees", viewallEmployees);
+		return "employee/list-employees";
+
+	}
+
+	@PostMapping("/saveEmployees")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		this.employeeservice.saveEmployee(employee);
+		return "redirect:/employees/listEmployees";
+
+	}
+
+	@PutMapping("/updateEmployee/{id}")
+	public String updateEmployee(Model model, @RequestParam("id") long id, Employee employee) {
+		Employee updateEmployee = this.employeeservice.updateEmployee(id, employee);
+		model.addAttribute("updateEmployee", updateEmployee);
+		return "";
+	}
+
+	@GetMapping("/add")
+	public String showFormForAdd(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+		return "employee/employee-form";
+	}
+}
